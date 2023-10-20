0e4ef124270011fcaa7a19c31a4c4f30249031cc
j-hazart <jessy.hazart@gmail.com>
2023-10-20 16:49:08 +0200

    add authentication documentation

--- dist/src/service/auth.js
+++ dist/src/service/auth.js
@@ -13,6 +13,12 @@ if (process.env.JWT_SECRET) {
 else {
     console.error("JWT_SECRET is not defined in your environment variables.");
 }
+/**
+ * The function getToken generates a JSON Web Token (JWT) using the provided email and a secret key.
+ * @param {string} email - The `email` parameter is a string that represents the email address of the
+ * user for whom the token is being generated.
+ * @returns a token, which is a string.
+ */
 function getToken(email) {
     const payload = { email };
     if (payload && jwtSecret) {
@@ -24,6 +30,18 @@ function getToken(email) {
     }
 }
 exports.getToken = getToken;
+/**
+ * The function `verifyToken` is used to verify the authorization token in the request header and
+ * throws an error if it is missing, has an incorrect type, or is not valid.
+ * @param {Request} req - The `req` parameter represents the HTTP request object, which contains
+ * information about the incoming request such as headers, query parameters, and request body.
+ * @param {Response} res - The `res` parameter is the response object that is used to send the response
+ * back to the client. It contains methods and properties that allow you to manipulate the response,
+ * such as setting the status code, sending data, or setting headers.
+ * @param {NextFunction} next - The `next` parameter is a function that is called to pass control to
+ * the next middleware function in the request-response cycle. It is typically used to move to the next
+ * middleware function or to the route handler.
+ */
 function verifyToken(req, res, next) {
     try {
         const authorizationHeader = req.get("Authorization");
